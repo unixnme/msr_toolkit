a07c7c39420875467ac1f29ed591eb30fef622c0
Unix&Me <unixnme@gmail.com>
2018-04-04 23:59:35 +0900

    initial commit

---
+++ create_lists.py
@@ -0,0 +1,57 @@
+import os
+import glob
+import numpy as np
+np.random.seed(0)
+
+# find all mfc files
+DIR = '../LibriSpeech'
+files = glob.glob(DIR + '/**/*.mfc', recursive=True)
+
+# collect by user
+speakers = {}
+for filepath in files:
+    spkr = filepath.split('/')[-2]
+    if spkr not in speakers:
+        speakers[spkr] = []
+    speakers[spkr].append(filepath)
+
+min_files = 0
+for spkr,entry in speakers.items():
+    min_files = min(min_files, len(entry))
+
+# choose N users for test / train
+N = len(speakers) // 10
+speaker_list = np.asarray(sorted(list(speakers.keys())))
+np.random.shuffle(speaker_list)
+
+# select M files for train
+train_files = {}
+test_files = {}
+M = min_files - 1
+for spkr in speaker_list[:N]:
+    # these are train/test speakers
+    files = speakers[spkr]
+    np.random.shuffle(files)
+    train_files[spkr] = files[:M]
+    test_files[spkr] = files[M:]
+
+ubm_files = {}
+for spkr in speaker_list[N:]:
+    # these are UBM speakers
+    files = speakers[spkr]
+    ubm_files[spkr] = files
+
+ubm_to_write = ''
+ubm_ind_to_write = ''
+for spkr,files in ubm_files.items():
+    for filepath in files:
+        ubm_to_write += filepath + '\n'
+        ubm_ind_to_write += spkr + ' ' + filepath + '\n'
+
+with open('ubm.lst', 'w') as f:
+    f.write(ubm_to_write)
+
+with open('ubm_ind.lst', 'w') as f:
+    f.write(ubm_ind_to_write)
+
+
